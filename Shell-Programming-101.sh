63ec901fe5f097be1275eccc87b977ac5c0e1792
Akshay Bhardwaj <akshaybhardwaj@akshays-air.home>
2024-09-29 23:42:56 -0400

    Added more commands

--- Shell-Programming-101.sh
+++ Shell-Programming-101.sh
@@ -4,11 +4,10 @@
 #Date: 9.29.2024
 
 
-#Creating functions
-Five_Dots()
+###---CREATING FUNCTIONS---###
+
+Three_Dots()
 {
-	Echo '.'
-	Echo '.'
 	Echo '.'
 	Echo '.'
 	Echo '.'
@@ -23,23 +22,26 @@ Print_Text()
 
 
 Echo 'Script is running now.'
-Five_Dots
+Three_Dots
 
 
-#Listing all file in a Directory
+###---LISTING ALL FILES IN A DIRECTORY---###
 
 Print_Text '[1/n] Listing all files in this directory using % ls:'
 ls
-Five_Dots
+Three_Dots
 
-#Creating a Directory
+###---CREATING A DIRECTORY---###
 
-Print_Text '[2/n] Creating a temporary directory using % mkdir <Directory_Name>'
-mkdir Temp_Directory
+Print_Text '[2/n] Creating temporary directory(s) using % mkdir <Directory_Name>'
+mkdir Temp_Directory Temp_Directory1 Temp_Directory2
 ls
-Five_Dots
+Print_Text '[2/n] Creating more temporary directory(s)'
+mkdir "Folder 1" 'Folder 2' Folder\ 3
+ls
+Three_Dots
 
-#Creating files in a Directory
+###---CREATING FILES IN A DIRECTORY---###
 
 Print_Text '[3/n] Entering in Temp_Directory using % cd Temp_Directory'
 cd Temp_Directory
@@ -53,14 +55,43 @@ ni F2.txt
 ni F3.txt
 ls
 cd ..
-Five_Dots
+Three_Dots
 
-#Deleting a directory
+###---DELETING A DIRECTORY---###
 
-Print_Text '[4/n] Deleting Temp_Directory now using % rmdir Temp_Directory'
-rmdir Temp_Directory
-Print_Text 'Listing Files and Folders now using % ls'
+Print_Text '[4/n] Deleting Temp_Directory and others using % rmdir Temp_Directory'
+rmdir Temp_Directory Temp_Directory1 Temp_Directory2 "Folder 1" 'Folder 2' Folder\ 3
+Print_Text 'Listing Files and Folders again using % ls'
 ls
-Five_Dots
+Three_Dots
+
+###---CREATING FILES---###
+
+Print_Text '[5/n] Creating a file using % ls > filename.txt'
+ls > Output1.txt
+Print_Text 'File Output1.txt is created'
+Print_Text '[5/n] Creating a file using % echo > filename.txt'
+echo "This is first line of Output2.txt file" > Output2.txt
+Print_Text 'File Output2.txt is created'
+Three_Dots
+
+###---DISPLAYING FILE CONTENT---###
+
+Print_Text '[6/n] Displaying files content using % cat Output1.txt Output2.txt'
+cat Output1.txt Output2.txt
+Echo ''
+Three_Dots
+rm Output1.txt Output2.txt
+
+###---PIPING---###
+
+Print_Text '[7/n] Holding data in memory using pipes |'
+Print_Text 'Pipes operate entirely in memory, and most Unix command line tools will expect to receive input from a pipe if you don’t specify a file for them to work on. Looking at the line above, you can see that it’s two commands, ls ~ (list the contents of the home directory) and wc -l (count the lines), separated by a vertical bar character (“|”). This process of piping one command into another is so commonly used that the character itself is often referred to as the pipe character, so if you see that term you now know it just means the vertical bar.'
+Echo ''
+Print_Text 'Get Number of files in a Directory using % ls and pipe | it with other command to do the count of lines using % wc -l'
+ls ~| wc -l
+Three_Dots
+
+###---CALLING AN API---###
 
 
